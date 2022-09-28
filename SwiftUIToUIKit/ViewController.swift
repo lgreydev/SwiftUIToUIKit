167b33a17f0d3aa96b17ccdfc9d5e5982c452261
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-09-28 17:44:43 +0300

    Setup button

--- SwiftUIToUIKit/ViewController.swift
+++ SwiftUIToUIKit/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
+    private let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupButton()
     }
-
-
 }
 
+
+private extension ViewController {
+    func setupButton() {
+        view.addSubview(button)
+        button.center = view.center
+        button.setTitle("Show Settings", for: .normal)
+        button.backgroundColor = .systemIndigo
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+}
