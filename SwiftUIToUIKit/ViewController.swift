06be2b95b7ec4c889b183cce15185ce0d47cff4f
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-09-28 17:05:52 +0300

    Initial Commit

---
+++ SwiftUIToUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftUIToUIKit
+//
+//  Created by Sergey Lukaschuk on 28.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
