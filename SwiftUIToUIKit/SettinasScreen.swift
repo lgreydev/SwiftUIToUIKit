5aefeb999134211209ba7b923b1ea8865df677eb
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-09-28 17:08:39 +0300

    Added SwiftUI screen

---
+++ SwiftUIToUIKit/SettinasScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SettinasScreen.swift
+//  SwiftUIToUIKit
+//
+//  Created by Sergey Lukaschuk on 28.09.2022.
+//
+
+import SwiftUI
+
+struct SettinasScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettinasScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SettinasScreen()
+    }
+}
